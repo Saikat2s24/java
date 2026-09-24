fa9d115f313018c54aacbc439608ee5e3f1e5045
Saikat2s24 <saikatsen0811@gmail.com>
2026-09-24 14:37:44 +0530

    Add samePckg Java program

---
+++ src/p1/samePckg.java
@@ -0,0 +1,35 @@
+package p1;
+class Tests
+{
+	public int x=25;
+	private int y=30;
+	protected int z=60;
+	 int d=45;
+	 public void show_x()
+	 {
+		 System.out.println("X:"+x);
+	 }
+	 private void show_y()
+	 {
+		 System.out.println("Y:"+y);
+	 }
+	 protected void show_z()
+	 {
+		 System.out.println("Z:"+z);
+	 }
+	 void show_d()
+	 {
+		 System.out.println("D:"+d);
+	 }
+}
+public class samePckg extends Tests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   samePckg sm=new samePckg();
+   sm.show_x();
+   sm.show_z();
+   sm.show_d();
+	}
+
+}
